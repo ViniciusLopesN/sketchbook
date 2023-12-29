502b13548351f14afcaa83a4f31f535eb34db1f8
Vinicius <65102990+ViniciusLopesN@users.noreply.github.com>
2023-12-29 18:40:47 -0300

    Added slider logic slider now correctly changes size of screen while maintaining proportions on "pixels"

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 const screen = document.querySelector('#screen');
+const canvasSize = document.getElementById('myRange');
+const sizeDisplay = document.getElementById('sizeDisplay');
+const colorPicker = document.getElementById('color-picker');
+
+canvasSize.value = 16;
+const color = colorPicker.value;
+
+function printScreen() {
+  let ratio = (100/(canvasSize.value)); 
+
+  sizeDisplay.textContent = `${canvasSize.value} x ${canvasSize.value}`;
+
+  for (let index = 0; index < (canvasSize.value*canvasSize.value); index++) {
+    const pixel = [];
+    pixel[index] = document.createElement('div');
+    pixel[index].classList.add('square');
+    screen.appendChild(pixel[index]);
+    };
+
+  const pixelInfo = document.querySelectorAll('.square');
+
+  for (const info of pixelInfo) {
+    info.style.cssText = `flex-basis: ${ratio}%`;
+    };
+}
+document.addEventListener('DOMContentLoaded',printScreen());
+
+canvasSize.addEventListener('input', () => {
+  document.getElementById('screen').innerHTML = ' '
+  printScreen();
+});
 
 
-for (let index = 0; index < (16*16); index++) {
-  const pixel = [];
-  pixel[index] = document.createElement('div');
-  pixel[index].classList.add('square');
-  document.getElementById('screen').appendChild(pixel[index]);
-  }
+
+
+// canvasSize.addEventListener('input', () => {
+//   sizeDisplay.textContent = `${canvasSize.value} x ${canvasSize.value}`;
+//   let ratio = (100/(canvasSize.value))
+//   for (const info of pixelInfo) {
+//     info.style.cssText = `flex-basis: ${ratio}%`;
+//     };
+// });
